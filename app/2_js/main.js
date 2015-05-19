fba270eaa5c1120beeed78b86aa41dccb08b7731
Guillaume Danzin <gda@triptyk.eu>
2015-05-19 22:40:11 +0200

    modifs slider

--- app/2_js/main.js
+++ app/2_js/main.js
@@ -2,6 +2,32 @@
 
     $(document).ready(function() {
 
+   //  	setInterval(simpleSlider, 10000);
+
+   //  	function simpleSlider() {
+
+			// 	var curImg = $('.panoramaSlider .current');
+			// 	var nextImg = curImg.next('div');
+			// 	var activeMenu = $('.panoramaSlider nav .active');
+			// 	var nextActiveMenu = activeMenu.next();
+
+			// 	if (nextImg.length == 0) {
+			// 		nextImg = $('.panoramaSlider .wideContainer > div:first');
+			// 	}
+
+			// 	if (nextActiveMenu.length == 0) {
+			// 		nextActiveMenu = $('.panoramaSlider nav li:first');
+			// 	}
+
+
+			// 	nextImg.animate({'right' : '0px'}, 500, function(){
+			// 		nextImg.addClass('current').removeAttr('style');
+			// 		curImg.removeClass('current');
+			// 		nextActiveMenu.addClass('active');
+			// 		activeMenu.removeClass('active');
+			// 	});
+
+			// }
 
     });
 
