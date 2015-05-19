a57d1807f24d439e601a6e1cc1b7c4154a1dd771
spawnytoutch <guillaumedanzin@gmail.com>
2015-05-19 23:57:33 +0200

    modifs slider

--- app/2_js/main.js
+++ app/2_js/main.js
@@ -1,34 +1,25 @@
 (function($) {
 
-    $(document).ready(function() {
+	$(document).ready(function() {
 
-   //  	setInterval(simpleSlider, 10000);
+		setInterval(simpleSlider, 5000);
 
-   //  	function simpleSlider() {
+		function simpleSlider() {
 
-			// 	var curImg = $('.panoramaSlider .current');
-			// 	var nextImg = curImg.next('div');
-			// 	var activeMenu = $('.panoramaSlider nav .active');
-			// 	var nextActiveMenu = activeMenu.next();
+			var curImg = $('.panoramaSlider .current');
+			var nextImg = curImg.next('div');
 
-			// 	if (nextImg.length == 0) {
-			// 		nextImg = $('.panoramaSlider .wideContainer > div:first');
-			// 	}
+			if (nextImg.length == 0) {
+				nextImg = $('.panoramaSlider > div:first');
+			}
 
-			// 	if (nextActiveMenu.length == 0) {
-			// 		nextActiveMenu = $('.panoramaSlider nav li:first');
-			// 	}
+			nextImg.animate({'right' : '0px'}, 500, function(){
+				nextImg.addClass('current').removeAttr('style');
+				curImg.removeClass('current');
+			});
 
+		}
 
-			// 	nextImg.animate({'right' : '0px'}, 500, function(){
-			// 		nextImg.addClass('current').removeAttr('style');
-			// 		curImg.removeClass('current');
-			// 		nextActiveMenu.addClass('active');
-			// 		activeMenu.removeClass('active');
-			// 	});
-
-			// }
-
-    });
+	});
 
 })(jQuery);
